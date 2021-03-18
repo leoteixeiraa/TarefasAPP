dd267c80a622943cd7414aeeb31ca84767c75b3a
Leonardo Teixeira <leovenancio00@gmail.com>
2021-03-18 19:40:55 -0300

    Input de data formato BR(pacote intl)e input de prioridade da tarefa

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TarefasAPP
 Aplicativo desenvolvido em flutter para gerenciar suas tarefas diárias
+
+### Pacotes utilizados
+intl 0.17.0 -> Para formatar a data no padrão BR
--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class AddTaskScreen extends StatefulWidget {
   @override
@@ -9,7 +10,28 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
   final _formKey = GlobalKey<FormState>();
   String _title = '';
   String _priority;
-  DateTime _dat = DateTime.now();
+  DateTime _date = DateTime.now();
+  TextEditingController _dateController = TextEditingController();
+
+  final DateFormat _dateFormatter = DateFormat('dd/MM/yyyy');
+  final List<String> _priorities = [
+    'Serio Legal Fazer, Importante, Super Importante'
+  ];
+
+  _handleDatePicker() async {
+    final DateTime date = await showDatePicker(
+      context: context,
+      initialDate: _date,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (date != null && date != _date) {
+      setState(() {
+        _date = date;
+      });
+      _dateController.text = _dateFormatter.format(date);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,16 +82,31 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                 Padding(
                   padding: EdgeInsets.symmetric(vertical: 20.0),
                   child: TextFormField(
+                    controller: _dateController,
+                    style: TextStyle(fontSize: 18.0),
+                    onTap: _handleDatePicker,
+                    decoration: InputDecoration(
+                      labelText: 'Data para entrega',
+                      labelStyle: TextStyle(fontSize: 18.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  child: TextFormField(
                     style: TextStyle(fontSize: 18.0),
                     decoration: InputDecoration(
-                      labelText: 'Titulo',
+                      labelText: 'Prioridade',
                       labelStyle: TextStyle(fontSize: 18.0),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10.0),
                       ),
                     ),
                     validator: (input) => input.trim().isEmpty
-                        ? 'Por Favor coloque um titulo para a tarefa'
+                        ? 'Por Favor selecione a prioridade desta tarefa'
                         : null,
                     onSaved: (input) => _title = input,
                     initialValue: _title,

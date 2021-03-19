1b8eaf3f7c42203d38b1c9b474b3e70d192c142b
Leonardo Teixeira <leovenancio00@gmail.com>
2021-03-19 00:37:32 -0300

    Botao adicionar e validaçoes no input

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # TarefasAPP
 Aplicativo desenvolvido em flutter para gerenciar suas tarefas diárias
 
+### Funcionalidades do Aplicativo
+Input de data é recarrega com a data atual
 ### Pacotes utilizados
 intl 0.17.0 -> Para formatar a data no padrão BR
+### Comentarios
+Foi utilizado a classe **SingleChildScrollView** para que o teclado não atrapalhe na digitação do usuario no input.
--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -15,8 +15,22 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
 
   final DateFormat _dateFormatter = DateFormat('dd/MM/yyyy');
   final List<String> _priorities = [
-    'Serio Legal Fazer, Importante, Super Importante'
+    'Seria Interessante Fazer',
+    'Importante',
+    'Super Importante'
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    _dateController.text = _dateFormatter.format(_date);
+  }
+
+  @override
+  void dispose() {
+    _dateController.dispose();
+    super.dispose();
+  }
 
   _handleDatePicker() async {
     final DateTime date = await showDatePicker(
@@ -33,89 +47,149 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
     }
   }
 
+  _submit() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      print('$_title, $_date, $_priority');
+
+      //Inserir Tarefas ao banco de dados
+
+      //Atualizar Tarefa
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          GestureDetector(
-            onTap: () => Navigator.pop(context),
-            child: Icon(
-              Icons.arrow_back,
-              size: 50.0,
-              color: Theme.of(context).primaryColor,
-            ),
-          ), //GestureDetector
-          SizedBox(height: 20.0),
-          Text(
-            'Adicionar Tarefa',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 40.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 10.0),
-          Form(
-            key: _formKey,
+      body: GestureDetector(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 80.0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20.0),
-                  child: TextFormField(
-                    style: TextStyle(fontSize: 18.0),
-                    decoration: InputDecoration(
-                      labelText: 'Titulo',
-                      labelStyle: TextStyle(fontSize: 18.0),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                    validator: (input) => input.trim().isEmpty
-                        ? 'Por Favor coloque um titulo para a tarefa'
-                        : null,
-                    onSaved: (input) => _title = input,
-                    initialValue: _title,
+                GestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: Icon(
+                    Icons.arrow_back,
+                    size: 50.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ), //GestureDetector
+                SizedBox(height: 20.0),
+                Text(
+                  'Adicionar Tarefa',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20.0),
-                  child: TextFormField(
-                    controller: _dateController,
-                    style: TextStyle(fontSize: 18.0),
-                    onTap: _handleDatePicker,
-                    decoration: InputDecoration(
-                      labelText: 'Data para entrega',
-                      labelStyle: TextStyle(fontSize: 18.0),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
+                SizedBox(height: 10.0),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: TextFormField(
+                          style: TextStyle(fontSize: 18.0),
+                          decoration: InputDecoration(
+                            labelText: 'Titulo',
+                            labelStyle: TextStyle(fontSize: 18.0),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          validator: (input) => input.trim().isEmpty
+                              ? 'Por Favor coloque um titulo para a tarefa'
+                              : null,
+                          onSaved: (input) => _title = input,
+                          initialValue: _title,
+                        ),
                       ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20.0),
-                  child: TextFormField(
-                    style: TextStyle(fontSize: 18.0),
-                    decoration: InputDecoration(
-                      labelText: 'Prioridade',
-                      labelStyle: TextStyle(fontSize: 18.0),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: TextFormField(
+                          readOnly: true,
+                          controller: _dateController,
+                          style: TextStyle(fontSize: 18.0),
+                          onTap: _handleDatePicker,
+                          decoration: InputDecoration(
+                            labelText: 'Data para entrega',
+                            labelStyle: TextStyle(fontSize: 18.0),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                    validator: (input) => input.trim().isEmpty
-                        ? 'Por Favor selecione a prioridade desta tarefa'
-                        : null,
-                    onSaved: (input) => _title = input,
-                    initialValue: _title,
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: DropdownButtonFormField(
+                          isDense: true,
+                          icon: Icon(Icons.arrow_drop_down_circle),
+                          iconSize: 22.0,
+                          iconEnabledColor: Theme.of(context).primaryColor,
+                          items: _priorities.map((String priority) {
+                            return DropdownMenuItem(
+                              value: priority,
+                              child: Text(
+                                priority,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                          style: TextStyle(fontSize: 18.0),
+                          decoration: InputDecoration(
+                            labelText: 'Prioridade',
+                            labelStyle: TextStyle(fontSize: 18.0),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          validator: (input) => _priority == null
+                              ? 'Por Favor selecione a prioridade desta tarefa'
+                              : null,
+                          onChanged: (value) {
+                            setState(() {
+                              _priority = value;
+                            });
+                          },
+                          value: _priority,
+                        ),
+                      ),
+                      Container(
+                          margin: EdgeInsets.symmetric(vertical: 20.0),
+                          height: 60.0,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          child: TextButton(
+                            //TextButton foi trocado por FlatButton
+                            child: Text(
+                              'Adicionar',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                              ),
+                            ),
+                            onPressed: _submit,
+                          ))
+                    ],
                   ),
-                ),
+                )
               ],
             ),
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
